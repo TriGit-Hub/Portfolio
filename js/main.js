e71e2a8061549353d882205102233dd406fda710
trigit-hub <cjtr.1998@gmail.com>
2021-06-09 18:47:23 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// ======= Mostrar y esconder menu //
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+
+//====== Mostrat =======// 
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+//===== Esconder =====//
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // cuando se elije una de las opciones del menu le quitamos la clase para que deje de mostrarse 
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
